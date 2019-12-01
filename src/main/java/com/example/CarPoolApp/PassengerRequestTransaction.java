07b9b49793df4821fe38fce86a2b717e4091b36b
lunde <lunde@Adept>
2019-12-01 17:38:25 -0600

    Updated Master

--- src/main/java/com/example/CarPoolApp/PassengerRequestTransaction.java
+++ src/main/java/com/example/CarPoolApp/PassengerRequestTransaction.java
@@ -3,8 +3,8 @@ package com.example.CarPoolApp;
 public class PassengerRequestTransaction {
 	private DatabaseQuery query;
 
-	public String savePassengerRequest(PassengerRequest newRequest) {
-		// Makes a new database query instance? Or should be static?
-		// Use instance to connect to database.
-	}
+//	public String savePassengerRequest(PassengerRequest newRequest) {
+//		// Makes a new database query instance? Or should be static?
+//		// Use instance to connect to database.
+//	}
 }
