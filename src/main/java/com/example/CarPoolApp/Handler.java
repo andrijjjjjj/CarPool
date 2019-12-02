56afa30ab0c86be2e65facc03e76e910353831e3
lunde <lunde@Adept>
2019-12-02 08:22:06 -0600

    Added basic user input

--- src/main/java/com/example/CarPoolApp/Handler.java
+++ src/main/java/com/example/CarPoolApp/Handler.java
@@ -5,35 +5,27 @@ package com.example.CarPoolApp;
 
 import java.util.ArrayList;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@Controller
 public class Handler {
 
-//	public ArrayList<RidePost> viewAllRides() {
-//		// Call the method getAllRidePosts() from RidePostTransaction.
-//		// Return the resulting arrayList of RidePosts.
-//		return RidePostTransaction.getAllRidePosts();
-//	}
-//
-//	public ArrayList<RidePost> viewAllRides(String restrictions) {
-//
-//	}
-//
-//	public String removeRidePost(int ridePostID) // confirmation
-//	{
-//
-//	}
-//
-//	public String cancelRide(int passengerRequestID) {
-//
-//	}
-//
-//	public String makePassengerRequest(int ridePostID, String passengerUsername) {
-//
-//	}
+	@GetMapping("/users")
+	public String enterLogin(Model model) {
+		model.addAttribute("users", new User());
+		return "users";
+	}
 
-	// Static classes arent static in here. They are their own objects/classes with
-	// static methods!
+	@PostMapping("/users")
+	public String exitLogin(@ModelAttribute User user) {
+		return "result";
+	}
+
 }
