2cf5cf0464643d6456fc472a570e85c163a06a85
Devitt, Michael <mjdevi1@ilstu.edu>
2019-12-05 00:11:56 -0600

    Yah

--- src/main/java/com/example/CarPoolApp/ObjectFactory.java
+++ src/main/java/com/example/CarPoolApp/ObjectFactory.java
@@ -9,5 +9,12 @@ public class ObjectFactory {
 //
 //		// Save to database using passengerRequestTransaction.
 //	}
+	public User createUser(String username, String password, String phoneNumber, String emailAddress, String firstName,
+			String lastName) {
+		// boolean validated=false;
+		// validate(username,password,phoneNumber,emailAddress,firstName,lastName);
+		Profile new_profile = new Profile(firstName, lastName, phoneNumber, emailAddress);
+		User new_user = new User(username, password, new_profile);
+		return new_user;
+	}
 }
- 
